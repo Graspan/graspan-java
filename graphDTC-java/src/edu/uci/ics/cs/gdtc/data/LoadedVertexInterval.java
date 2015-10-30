e36d59d30d46c01ce4feacd09f4d8ea9bbadfde2
AftabHussain <aftab.hussain46@gmail.com>
2015-10-29 17:45:58 -0700

    added kai's loadedvertexinterval in data package

---
+++ graphDTC-java/src/edu/uci/ics/cs/gdtc/data/LoadedVertexInterval.java
@@ -0,0 +1,60 @@
+package edu.uci.ics.cs.gdtc.data;
+
+/**
+ * @author Kai Wang
+ *
+ * Created by Oct 28, 2015
+ */
+public class LoadedVertexInterval {
+	private int firstVertex;
+	private int lastVertex;
+	private int indexStart;
+	private int indexEnd;
+	private int partitionId;
+	
+	public LoadedVertexInterval(int firstVertex, int lastVertex, int partitionId) {
+		this.firstVertex = firstVertex;
+		this.lastVertex = lastVertex;
+		this.partitionId = partitionId;
+	}
+	
+	public void setFirstVertex(int firstVertex) {
+		this.firstVertex = firstVertex;
+	}
+	
+	public int getFirstVertex() {
+		return firstVertex;
+	}
+	
+	public void setLastVertex(int lastVertex) {
+		this.lastVertex = lastVertex;
+	}
+	
+	public int getLastVertex() {
+		return lastVertex;
+	}
+	
+	public void setIndexStart(int indexStart) {
+		this.indexStart = indexStart;
+	}
+	
+	public int getIndexStart() {
+		return indexStart;
+	}
+	
+	public void setIndexEnd(int indexEnd) {
+		this.indexEnd = indexEnd;
+	}
+	
+	public int getIndexEnd() {
+		return indexEnd;
+	}
+	
+	public void setPartitionId(int partitionId) {
+		this.partitionId = partitionId;
+	}
+	
+	public int getPartitionId() {
+		return partitionId;
+	}
+}
