e33835105c0621dd384f4898fd932695d10d443b
AftabHussain <aftab.hussain46@gmail.com>
2015-10-30 00:36:51 -0700

    added partsizes to allpartitions.java

--- graphDTC-java/src/edu/uci/ics/cs/gdtc/data/AllPartitions.java
+++ graphDTC-java/src/edu/uci/ics/cs/gdtc/data/AllPartitions.java
@@ -9,6 +9,7 @@ public class AllPartitions {
 	 */
 	public static int[] partAllocTable;
 
+	public static long[] partSizes;
 
 	/**
 	 * Initializes the partition allocation table
@@ -26,5 +27,22 @@ public class AllPartitions {
 	 */
 	public static int[] getPartAllocTab() {
 		return partAllocTable;
+	}
+
+	/**
+	 * 
+	 * @param arr
+	 */
+	public static void setPartSizes(long[] arr) {
+		partSizes = arr;
+	}
+
+	/**
+	 * Returns a ref to partSizes
+	 * 
+	 * @return
+	 */
+	public static long[] getPartSizes() {
+		return partSizes;
 	}
 }
