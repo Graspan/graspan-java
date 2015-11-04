39aaf48e4a2bca7019a3aa0993abebb5b9db54da
AftabHussain <aftab.hussain46@gmail.com>
2015-11-03 23:09:18 -0800

    Completed separation of newparts from parts already loaded

--- graphDTC-java/src/edu/uci/ics/cs/gdtc/partitionloader/PartitionLoader.java
+++ graphDTC-java/src/edu/uci/ics/cs/gdtc/partitionloader/PartitionLoader.java
@@ -57,22 +57,33 @@ public class PartitionLoader {
 		// get the scheduling info
 		readSchedulingInfo();
 
-		// initialize variables for partition loading
-		initLoadedPartDataStructs();
+		// initialize variables for partition loading based on number of
+		// partitions to load.
+		preliminaryInit();
 
+		// scheduler info test
 		// SchedulerInfo.printData();
 	}
 
 	/**
-	 * Initializes the data structures for loaded partitions.
+	 * Initialize variables for partition loading only based on number of
+	 * partitions to load.
 	 */
-	private void initLoadedPartDataStructs() {
+	private void preliminaryInit() {
 
+		int newParts[] = new int[UserInput.getNumPartsPerComputation()];
 		int loadedParts[] = new int[UserInput.getNumPartsPerComputation()];
+
+		for (int i = 0; i < loadedParts.length; i++) {
+			newParts[i] = -1;
+			loadedParts[i] = -1;
+		}
+
 		int loadedPartOutDegs[][] = new int[UserInput.getNumPartsPerComputation()][];
 		int loadedPartEdges[][][] = new int[UserInput.getNumPartsPerComputation()][][];
 		byte loadedPartEdgeVals[][][] = new byte[UserInput.getNumPartsPerComputation()][][];
 
+		LoadedPartitions.setLoadedParts(newParts);
 		LoadedPartitions.setLoadedParts(loadedParts);
 		LoadedPartitions.setLoadedPartOutDegs(loadedPartOutDegs);
 		LoadedPartitions.setLoadedPartEdges(loadedPartEdges);
@@ -93,16 +104,19 @@ public class PartitionLoader {
 		}
 		logger.info("Loading partitions : " + str + "...");
 
-		// get the degrees of the source vertices in the partitions
-		// getDegrees(baseFilename, partsToLoad);
-		LoadedPartitions.setLoadedPartOutDegs(getLoadedPartDegs(baseFilename, partsToLoad));
+		// update newPartsToLoad
+		updateNewParts(partsToLoad);
+
+		// update loadedPartOutDegrees
+		updateDegsOfPartsToLoad();
 
 		// initialize data structures of the partitions to load
-		initPartDataStructs(partsToLoad);
+		// NEED TO CHANGE THIS
+		initVarsOfPartsToLoad(partsToLoad);
 
 		logger.info("Initialized data structures for partitions to load.");
 		// fill the partition data structures
-		fillPartDataStructs(baseFilename, partsToLoad);
+		fillVarsOfPartsToLoad(partsToLoad);
 
 		int loadedPartOutDegs[][] = LoadedPartitions.getLoadedPartOutDegs();
 		int partEdges[][][] = LoadedPartitions.getLoadedPartEdges();
@@ -119,6 +133,8 @@ public class PartitionLoader {
 		logger.info("Sorted loaded partitions.");
 
 		LoadedPartitions.setLoadedParts(partsToLoad);
+
+		// loaded partitions test
 		// LoadedPartitions.printData();
 	}
 
@@ -216,42 +232,144 @@ public class PartitionLoader {
 		return intervals;
 	}
 
+	private void updateNewParts(int partsToLoad[]) {
+		/*
+		 * Initialize the degrees array for each new partition to load. We shall
+		 * not reinitialize the degrees of the partitions that have already been
+		 * loaded. In addition, at no point will partsToLoad be equal to
+		 * loadedparts
+		 */
+		int[] loadedParts = LoadedPartitions.getLoadedParts();
+		int[] newParts = LoadedPartitions.getNewParts();
+
+		/*
+		 * partid loading test 1/3 (comment loadedParts, newParts initialization
+		 * above, change name of parameter partsToLoad above)
+		 * 
+		 */
+		// int[] loadedParts = { 5, 7, 10, 1, 101 };
+		// int[] newParts = { -1, -1, -1, -1, -1 };
+		// int[] partsToLoad = { 6, 5, 7, 10, 2 };
+		// System.out.println("START");
+		// System.out.println("loadedParts");
+		// for (int i = 0; i < loadedParts.length; i++)
+		// System.out.print(loadedParts[i] + " ");
+		// System.out.println();
+		// System.out.println("newParts");
+		// for (int i = 0; i < newParts.length; i++)
+		// System.out.print(newParts[i] + " ");
+		// System.out.println();
+		// System.out.println("partsToLoad");
+		// for (int i = 0; i < partsToLoad.length; i++)
+		// System.out.print(partsToLoad[i] + " ");
+		// System.out.println();
+
+		// check whether the requested partition for loading is already loaded
+		for (int i = 0; i < partsToLoad.length; i++) {
+			for (int j = 0; j < loadedParts.length; j++) {
+				if (partsToLoad[i] == loadedParts[j]) {
+					// clear parts already loaded in partsToLoad
+					partsToLoad[i] = -1;
+					// mark old parts in loadedParts
+					loadedParts[j] = -loadedParts[j];
+				}
+			}
+		}
+
+		/*
+		 * partid loading test 2/3
+		 */
+		// System.out.println("AFTER MARKING NEW PART IDS");
+		// System.out.println("loadedParts");
+		// for (int i = 0; i < loadedParts.length; i++)
+		// System.out.print(loadedParts[i] + " ");
+		// System.out.println();
+		// System.out.println("newParts");
+		// for (int i = 0; i < newParts.length; i++)
+		// System.out.print(newParts[i] + " ");
+		// System.out.println();
+		// System.out.println("partsToLoad");
+		// for (int i = 0; i < partsToLoad.length; i++)
+		// System.out.print(partsToLoad[i] + " ");
+		// System.out.println();
+
+		// store the ids of the new partitions to load in newParts to preserve
+		// order. (Note: order is important as other variables like
+		// loadedPartdegs, partEdges, etc use order to store values.)
+		for (int i = 0; i < partsToLoad.length; i++) {
+			if (partsToLoad[i] != -1) {
+				// found new partid
+				for (int j = 0; j < loadedParts.length; j++) {
+					if (loadedParts[j] > 0) {
+						// found loaded partid to be discarded
+						loadedParts[j] = -loadedParts[j];
+						// store new partid in the corresponding index in
+						// newParts
+						newParts[j] = partsToLoad[i];
+						break;
+					}
+				}
+			}
+		}
+
+		// getting newparts in loadedparts
+		for (int i = 0; i < newParts.length; i++) {
+			if (newParts[i] != -1) {
+				loadedParts[i] = newParts[i];
+			}
+			if (loadedParts[i] < 0) {
+				loadedParts[i] = -loadedParts[i];
+			}
+		}
+
+		/*
+		 * partid loading test 3/3
+		 */
+		// System.out.println("AFTER STORING NEW PARTS");
+		// System.out.println("loadedParts");
+		// for (int i = 0; i < loadedParts.length; i++)
+		// System.out.print(loadedParts[i] + " ");
+		// System.out.println();
+		// System.out.println("newParts");
+		// for (int i = 0; i < newParts.length; i++)
+		// System.out.print(newParts[i] + " ");
+		// System.out.println();
+		// System.out.println("partsToLoad");
+		// for (int i = 0; i < partsToLoad.length; i++)
+		// System.out.print(partsToLoad[i] + " ");
+		// System.out.println();
+		// System.exit(0);
+
+	}
+
 	/**
-	 * Gets the degrees of the source vertices of the partitions that are to be
-	 * loaded.
+	 * Updates the degrees of the source vertices of the partitions that are to
+	 * be loaded.
 	 * 
 	 * @param baseFilename
 	 * @param partsToLoad
 	 * @throws IOException
 	 * @throws NumberFormatException
 	 */
-	private int[][] getLoadedPartDegs(String baseFilename, int[] partsToLoad) throws IOException {
+	private void updateDegsOfPartsToLoad() throws IOException {
 
-		/*
-		 * Initialize the degrees array for each partition
-		 */
+		int[] newParts = LoadedPartitions.getNewParts();
+		int[][] partOutDegs = LoadedPartitions.getLoadedPartOutDegs();
 
-		// initialize Dimension 1 (Total no. of Partitions)
-		int[][] partOutDegs = new int[partsToLoad.length][];// TODO will need to
-															// change this,
-															// can't
-															// reinitialize the
-															// whole partOutDegs
-															// Array
-
-		for (int i = 0; i < partsToLoad.length; i++) {
-
-			// initialize Dimension 2 (Total no. of Unique SrcVs for a
-			// Partition)
-			partOutDegs[i] = new int[PartitionQuerier.getNumUniqueSrcs(partsToLoad[i])];
+		for (int i = 0; i < newParts.length; i++) {
+			if (newParts[i] != -1) {
+				// initialize Dimension 2 (Total no. of Unique SrcVs for a
+				// Partition)
+				partOutDegs[i] = new int[PartitionQuerier.getNumUniqueSrcs(newParts[i])];
+			}
 		}
 
 		/*
 		 * Scan degrees file of each partition
 		 */
-		for (int i = 0; i < partsToLoad.length; i++) {
+		for (int i = 0; i < newParts.length; i++) {
 			BufferedReader outDegInStrm = new BufferedReader(new InputStreamReader(
-					new FileInputStream(new File(baseFilename + ".partition." + partsToLoad[i] + ".degrees"))));
+					new FileInputStream(new File(baseFilename + ".partition." + newParts[i] + ".degrees"))));
 
 			String ln;
 			while ((ln = outDegInStrm.readLine()) != null) {
@@ -262,13 +380,12 @@ public class PartitionLoader {
 				int srcVId = Integer.parseInt(tok[0]);
 				int deg = Integer.parseInt(tok[1]);
 
-				partOutDegs[i][srcVId - PartitionQuerier.getMinSrc(partsToLoad[i])] = deg;
+				partOutDegs[i][srcVId - PartitionQuerier.getMinSrc(newParts[i])] = deg;
 			}
 			outDegInStrm.close();
 
-			logger.info("Loaded " + baseFilename + ".partition." + partsToLoad[i] + ".degrees" + partsToLoad[i]);
+			logger.info("Loaded " + baseFilename + ".partition." + newParts[i] + ".degrees" + newParts[i]);
 		}
-		return partOutDegs;
 
 	}
 
@@ -338,7 +455,7 @@ public class PartitionLoader {
 	/**
 	 * Initializes data structures of the partitions to load
 	 */
-	private void initPartDataStructs(int[] partsToLoad) {
+	private void initVarsOfPartsToLoad(int[] partsToLoad) {
 
 		int[][] partOutDegs = LoadedPartitions.getLoadedPartOutDegs();
 
@@ -384,7 +501,7 @@ public class PartitionLoader {
 	 * @param partInputStream
 	 * @throws IOException
 	 */
-	private void fillPartDataStructs(String baseFilename, int[] partsToLoad) throws IOException {
+	private void fillVarsOfPartsToLoad(int[] partsToLoad) throws IOException {
 
 		int[][][] partEdges = LoadedPartitions.getLoadedPartEdges();
 		byte[][][] partEdgeVals = LoadedPartitions.getLoadedPartEdgeVals();
