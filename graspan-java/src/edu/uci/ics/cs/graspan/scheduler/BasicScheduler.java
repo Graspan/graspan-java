aabf515258ed1312fc6ec105865823a0f0ebc5e4
AftabHussain <aftab.hussain46@gmail.com>
2015-12-07 14:53:18 -0800

    Merged Kai's Scheduler

--- graspan-java/src/edu/uci/ics/cs/graspan/scheduler/BasicScheduler.java
+++ graspan-java/src/edu/uci/ics/cs/graspan/scheduler/BasicScheduler.java
@@ -1,5 +1,10 @@
 package edu.uci.ics.cs.graspan.scheduler;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import edu.uci.ics.cs.graspan.datastructures.GlobalParams;
 
 /**
@@ -14,12 +19,11 @@ import edu.uci.ics.cs.graspan.datastructures.GlobalParams;
  */
 public class BasicScheduler implements IScheduler {
 
+	// // We are assuming at most 50 - numOfOriginalParts will be created after
+	// the
+	// // complete process
+	// private static final int SizeOfPartScheduleMap = 50;
 
-
-//	// We are assuming at most 50 - numOfOriginalParts will be created after the
-//	// complete process
-//	private static final int SizeOfPartScheduleMap = 50;
-	
 	private int[][] partTerminationMap = new int[GlobalParams.getNumParts()][GlobalParams.getNumParts()];
 
 	/**
@@ -80,6 +84,27 @@ public class BasicScheduler implements IScheduler {
 			return true;
 		else
 			return false;
+	}
+
+	public void generateComputationPairs() {
+		List<Integer> lst = new ArrayList<Integer>();
+		lst.add(1);
+		lst.add(2);
+		lst.add(3);
+		lst.add(4);
+		lst.add(5);
+		
+
+		Set<Set<Integer>> pairSet = new HashSet<Set<Integer>>();
+		Set<Integer> pair = new HashSet<Integer>();
+
+		for (int i = 0; i < lst.size(); i++) {
+			for (int j = i + 1; j < lst.size(); j++) {
+				pair.add(lst.get(i));
+				pair.add(lst.get(j));
+			}
+		}
+
 	}
 
 }
