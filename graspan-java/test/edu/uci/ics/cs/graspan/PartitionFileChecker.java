51abc6ed76a512276e3f10dc30cfaf8fbbfb54aa
aftabhussain <aftab.hussain46@gmail.com>
2016-04-12 16:42:41 -0700

    Partition file checker - print info for one vertex

--- graspan-java/test/edu/uci/ics/cs/graspan/PartitionFileChecker.java
+++ graspan-java/test/edu/uci/ics/cs/graspan/PartitionFileChecker.java
@@ -20,7 +20,13 @@ public class PartitionFileChecker {
 	 */
 	public static void main(String args[]) throws IOException {
 		String baseFilename = args[0];
-		readPartitionFile(baseFilename);
+		int reqVertexId = Integer.parseInt(args[1]);
+
+		for (int i = 0; i < 10; i++) {
+			// readPartitionFile(baseFilename);
+			System.out.println("partition " + i);
+			readPartitionFile(baseFilename,reqVertexId);
+		}
 	}
 
 	/**
@@ -29,29 +35,44 @@ public class PartitionFileChecker {
 	 * @param baseFilename
 	 * @throws IOException
 	 */
-	private static void readPartitionFile(String baseFilename) throws IOException {
+	private static void readPartitionFile(String baseFilename, int reqVId) throws IOException {
 		DataInputStream dataIn = new DataInputStream(new BufferedInputStream(new FileInputStream(baseFilename)));
-
+		int srcVId;
 		while (true) {
 			try {
-				// srcVId
-				System.out.println("src "+dataIn.readInt());
-				System.out.println("-----");
+				
+				srcVId = dataIn.readInt();
+			
+				if (srcVId==reqVId)
+				{
+					// srcVId
+					System.out.println("src " + srcVId);
+					System.out.println("-----");
 
-				// count (number of destVs from srcV in the current list)
-				int count = dataIn.readInt();
-				System.out.println("# "+count);
-				System.out.println("-----");
+					// count (number of destVs from srcV in the current list)
+					int count = dataIn.readInt();
+					System.out.println("# " + count);
+					System.out.println("-----");
 
-				for (int i = 0; i < count; i++) {
-					// dstVId
-					System.out.println("dst "+dataIn.readInt());
+					for (int i = 0; i < count; i++) {
+						// dstVId
+						System.out.println("dst " + dataIn.readInt());
 
-					// edge value
-					System.out.println("val "+dataIn.readByte());
-					
+						// edge value
+						System.out.println("val " + dataIn.readByte());
+
+					}
+					System.out.println("=====");
 				}
-				System.out.println("=====");
+				else{
+					// count (number of destVs from srcV in the current list)
+					int count = dataIn.readInt();
+
+					for (int i = 0; i < count; i++) {
+						dataIn.readInt();
+						dataIn.readByte();
+					}
+				}
 
 			} catch (Exception exception) {
 				break;
