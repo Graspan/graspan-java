da450d455d57d54c54cc60111f9c513694286b53
aftabhussain <aftab.hussain46@gmail.com>
2016-04-19 13:50:45 -0700

    refactoring Scheduler

--- graspan-java/src/edu/uci/ics/cs/graspan/scheduler/Scheduler.java
+++ graspan-java/src/edu/uci/ics/cs/graspan/scheduler/Scheduler.java
@@ -16,7 +16,7 @@ public class Scheduler {
 	private List<PartitionEdgeInfo> allEdgeInfo = new ArrayList<PartitionEdgeInfo>();
 	private List<Long> partitionNumEdges = new ArrayList<Long>();
 	private List<LoadedVertexInterval> intervals = null;
-	private static final Logger logger = GraspanLogger.getLogger("scheduler");
+	private static final Logger logger = GraspanLogger.getLogger("Scheduler");
 
 	// for temp use
 	public static int counterOne = 0;
@@ -77,63 +77,6 @@ public class Scheduler {
 		return intervals;
 	}
 
-	// unused
-	// private void computePriority() {
-	// assert(allEdgeInfo != null && partitionNumEdges != null);
-	// assert(allEdgeInfo.size() > 0 && partitionNumEdges.size() > 0);
-	// int size = allEdgeInfo.size();
-	//
-	// // each partition has an edgeInfo, iterate all partitions
-	// for(PartitionEdgeInfo edgeInfo : allEdgeInfo) {
-	// int partitionId = edgeInfo.getPartitionId();
-	// // get edgeInfo and priorityInfo for each partition
-	// List<Long> pEdgeInfo = edgeInfo.getPartitionEdgeInfo();
-	// // List<Long> priorityInfo = edgeInfo.getPriorityInfo();
-	//
-	// // compute priority, starting from (partitionId + 1)
-	// // for example, if there are 3 partitions, 0, 1, 2,
-	// // the current partition is 0, then consider(p0, p1), (p0, p2)
-	// for(int i = (partitionId + 1); i < size; i++) {
-	// // get edgeInfo for the current partition
-	// long edgesCurrentPartition = pEdgeInfo.get(partitionId);
-	// // get edgeInfo for the next partition
-	// long edgesNextPartition = pEdgeInfo.get(i);
-	// // compute priority and set priority
-	// // priorityInfo.set(i, edgesCurrentPartition + edgesNextPartition);
-	// }
-	// }
-	//
-	// }
-
-	/**
-	 * Unused Description: select two partitions to load
-	 */
-	// public List<Integer> schedulePartitions() {
-	// computePriority();
-	// long maxPriority = 0L;
-	// int scheduledOne = -1;
-	// int scheduledTwo = -1;
-	//
-	// // schedule two partitions every time
-	// List<Integer> result = new ArrayList<Integer>(2);
-	//
-	// // each partition has an edgeInfo
-	// for(PartitionEdgeInfo edgeInfo : allEdgeInfo) {
-	// int partitionId = edgeInfo.getPartitionId();
-	// scheduledOne = partitionId;
-	// // List<Long> priorityInfo = edgeInfo.getPriorityInfo();
-	// for(int i = 0; i < priorityInfo.size(); i++) {
-	// if(priorityInfo.get(i) > maxPriority) {
-	// maxPriority = priorityInfo.get(i);
-	// scheduledTwo = i;
-	// }
-	// }
-	// }
-	//
-	// result.add(scheduledOne);
-	// result.add(scheduledTwo);
-	// return result;
-	// }
 
 	/**
 	 * Sequential Scheduler (Basic)
@@ -186,8 +129,7 @@ public class Scheduler {
 			for (int j = 0; j < numOfPartitions; j++) {
 
 				// OPTION 1-----
-				edcPercentage[i][j] = (double) SchedulerInfo.getEdgeDestCount()[i][j]
-						/ partA_Size;
+				edcPercentage[i][j] = (double) SchedulerInfo.getEdgeDestCount()[i][j] / partA_Size;
 				// the remaining implementation needs to be changed if you want
 				// to
 				// implement this one
